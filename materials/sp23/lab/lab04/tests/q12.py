c687d3be8b9730492175a64267a66a05639d2c98
Default Jupyter user <jweichert@berkeley.edu>
2023-02-06 16:01:11 -0800

    releasing lab04

---
+++ materials/sp23/lab/lab04/tests/q12.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q12',
+    'points': None,
+    'suites': [   {   'cases': [{'code': ">>> # Make sure you're using a_proportion!\n>>> np.isclose(a_percentage, 70.71067811865476)\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
