b7ce0402359dbee8be8f6d8ad3abeaaab4b371ee
Default Jupyter user <jweichert@berkeley.edu>
2023-02-10 13:47:51 -0800

    releasing project1

---
+++ materials/sp23/project/project1/tests/q1_1.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q1_1',
+    'points': [0, 0],
+    'suites': [   {   'cases': [   {'code': ">>> # Check your column labels and spelling\n>>> p_pop.labels == ('time', 'population_total')\nTrue", 'hidden': False, 'locked': False},
+                                   {   'code': '>>> # Times should range from 1900 through 2020\n>>> all(p_pop.sort("time").column("time") == np.arange(1900, 2021))\nTrue',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/sp23/project/project1/tests/q1_7.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q1_7',
+    'points': [0, 0],
+    'suites': [   {   'cases': [   {'code': '>>> # Make sure you are using the date range 1950-2020\n>>> poland_since_1950.num_rows\n71', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> # Check your column labels and spelling\n'
+                                               ">>> all([label in poland_since_1950.labels for label in ['Children per woman', 'Child deaths per 1000 born']])\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
