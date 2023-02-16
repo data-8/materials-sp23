7e7bdc34e70f500b8df4adb2938246d174898575
Oscar Bjorkman <oscarb@berkeley.edu>
2023-02-16 11:00:48 -0800

    releasing hw05

---
+++ materials/sp23/hw/hw05/tests/q0_1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q0_1',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': '>>> # Make sure that your answer is a string\n>>> type(secret_phrase) == str \nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
