7fe4190d398264f32fbfd45c9fe74d0354d7eb2e
Default Jupyter user <jweichert@berkeley.edu>
2023-02-27 13:48:32 -0800

    releasing lab06

---
+++ materials/sp23/lab/lab06/tests/q1_10.py
@@ -0,0 +1,1 @@
+test = {'name': 'q1_10', 'points': None, 'suites': [{'cases': [{'code': '>>> peer_talk == True\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
