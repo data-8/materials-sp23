043adb4308f58f1e81e547b65583a4a6eb4e3a6b
Oscar Bjorkman <oscarb@berkeley.edu>
2023-03-01 10:36:00 -0800

    releasing hw07

---
+++ materials/sp23/hw/hw07/tests/q3_10.py
@@ -0,0 +1,1 @@
+test = {'name': 'q3_10', 'points': [], 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ materials/sp23/hw/hw07/tests/q3_8.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_8',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> type(correct_q8) == str\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
