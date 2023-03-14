98ac04655a5fd98cadabd67a555c52889bbd34d5
Default Jupyter user <jweichert@berkeley.edu>
2023-03-13 18:25:17 -0700

    releasing lab07

---
+++ materials/sp23/lab/lab07/tests/q2_12.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2_12',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> # Make sure secret_word is assigned to a string!\n>>> type(secret_word) == str\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> secret_word == "doggo8"\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/sp23/lab/lab07/tests/q2_5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2_5', 'points': None, 'suites': [{'cases': [{'code': '>>> test_option == 4\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
