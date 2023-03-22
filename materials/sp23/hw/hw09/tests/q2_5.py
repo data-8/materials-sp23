401f7e88bc7e7369f9d7b06f053e5a0c3bd74e23
Oscar Bjorkman <oscarb@berkeley.edu>
2023-03-22 10:57:44 -0700

    releasing hw09

---
+++ materials/sp23/hw/hw09/tests/q2_5.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2_5',
+    'points': [0, 0],
+    'suites': [   {   'cases': [{'code': '>>> type(option) == int\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 1 <= option <= 4\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/sp23/hw/hw09/tests/q3_6.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q3_6',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> -3 <= michelle_sample_mean_sd <= 3\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
