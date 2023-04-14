be2442bc1706d1548274894da8f77ed1da86b90e
Default Jupyter user <jweichert@berkeley.edu>
2023-04-14 14:43:16 -0700

    releasing project3

---
+++ materials/sp23/project/project3/tests/q1_1_2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_1_2',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': '>>> (type(most_stem) == str) | (type(most_stem) == np.str_)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ materials/sp23/project/project3/tests/q1_1_3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1_1_3',
+    'points': [0],
+    'suites': [   {   'cases': [{'code': '>>> (type(longest_uncut) == str) | (type(longest_uncut) == np.str_)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
